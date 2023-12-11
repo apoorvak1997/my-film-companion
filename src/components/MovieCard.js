9eef44a8a7ea51e0477cc6d4532e155430523bd3
apoorvak1997 <45688556+apoorvak1997@users.noreply.github.com>
2023-12-11 18:10:04 -0500

    Movie details styling

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,32 +1,66 @@
 import React, { useState } from 'react';
 import '../styles/moviecard.css';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import Favorite from '../components/Favorite';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import { CardActionArea } from '@mui/material';
+import MovieCardDetail from '../components/MovieCardDetail';
 
 
 export default function MovieCard(props) {
     const { title, overview, release_date, poster_path, id, isFavorite } = props.movie;
-    const [details, setDetails] = useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [movieDetail, setMovieDetail] = React.useState({});
+    const handleOpen = (e) => {
+        setMovieDetail(e);
+        setOpen(true);}
+    const handleClose = () => setOpen(false);
 
-    
-    function handleDetails() { setDetails(prevState => !prevState) }
 
 
     const baseImgUrl = "https://image.tmdb.org/t/p"
     const size = "w300"
     return (
+    <Card className ="movie-container">
+        
+        
+        <CardActionArea onClick={() =>handleOpen(props.movie)}>
+        
+        <CardMedia
+        component="img"
+        alt="img"
+        height="380"
+        image={`${baseImgUrl}/${size}${poster_path}`}
+        sx={{  objectFit: "inherit" }}
+        value={props.movie}
+      />
+      </CardActionArea>
+      <CardContent className ="movie-content">
+        <div className = "title-year">
 
-        <div className='movie-container'>
-            {details && <p> {overview} </p>}
-            <img src={`${baseImgUrl}/${size}${poster_path}`} alt="img" onClick={handleDetails}/>
-            <div className='movie-list'>
-            <ul className='movie-name'>
-                <li className='movie-item'>{title}</li>
-                <li className='movie-item'>{release_date.slice(0,4)}</li>
-            </ul>
-            <Favorite handleClick={()=>props.handleFavorite(id)} isFavorite={isFavorite}/>
-            </div>
+        
+
+
+        <Button className="button-title" onClick={() =>handleOpen(props.movie)}><Typography gutterBottom component="p" className="movie-title" href="">
+        {title}
+        </Typography>
+        </Button>
+        <MovieCardDetail open={open} onClose={handleClose} movieDetail={movieDetail}/>
+        <Typography className = "release-date" variant="body2" color="text.secondary">
+        {release_date.slice(0,4)}
+        </Typography>
         </div>
+        <div>
+        <Favorite handleClick={()=>props.handleFavorite(id)} isFavorite={isFavorite}/>
+        </div>
+      </CardContent>
+      
+    </Card>
 
     )
 
---
+++ src/components/MovieCardDetail.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import StarIcon from '@mui/icons-material/Star';
+import '../styles/details.css';
+
+export default function MovieCard(props) {
+    const {title,overview, vote_average, vote_count} = props.movieDetail;
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+    return (
+        <Modal
+        open={props.open}
+        onClose={props.onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+        <Box sx={style}>
+        <div className="modal-top">
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+        {title}
+        </Typography>
+        <div class = 'rating'>
+        <StarIcon sx={{fontSize : "2.5em" ,marginRight: "8px"}}/>
+        <div class = 'average-count'>
+        <Typography id="modal-modal-title-count" variant="h6" component="h2">
+         <strong>{(Math.round(vote_average * 10)/10).toFixed(1)} </strong>/10 
+        </Typography>
+        <Typography className ="vote-count">{vote_count} Votes</Typography>
+        </div>
+        </div>
+        </div>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+        {overview}
+        </Typography>
+        </Box>
+        </Modal>
+    )
+}
