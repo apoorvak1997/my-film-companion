672823fc9b8d5f6c1b09c1546780468d83c52e33
apoorvak1997 <45688556+apoorvak1997@users.noreply.github.com>
2023-12-12 10:39:41 -0500

    Fixing navbar issues, replacing with Material UI AppBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,69 @@
-import React from "react";
-import '../styles/navbar.css';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Menu, MenuItem, Typography, Hidden } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-export default function NavBar() {
-    return(
-        <nav className="navbar">
-      <ul className="menu_list">
-         <li><Link  to="/">Home </Link></li>
-          <li><Link to="/favorites">My Favorites</Link></li>
-        </ul>
-    </nav>
-    );
-}
+const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const location = useLocation();
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <AppBar position="static" style={{backgroundColor:"#f1f1f1"}}>
+      <Toolbar>
+        
+        <Hidden mdUp>
+          {/* Hamburger icon on screens < md */}
+          <IconButton
+            size="large"
+            edge="end"
+            color="black"
+            aria-label="menu"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="navbar-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleMenuClose}>
+            <Link to="/" style={{ textDecoration: 'none', color: 'black', textTransform: 'uppercase', fontWeight: location.pathname === '/' ? 'bold' : 'normal' }}>
+                Home
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose}>
+            <Link to="/favorites" style={{ textDecoration: 'none', color: 'black', textTransform: 'uppercase', fontWeight: location.pathname === '/favorites' ? 'bold' : 'normal' }}>
+                My Favorites
+              </Link>
+            </MenuItem>
+          </Menu>
+        </Hidden>
+        <Hidden  mdDown>
+          {/* Links on screens >= md */}
+          <Typography variant="h6" style={{ marginRight: '1rem' }}>
+          <Link to="/" style={{ textDecoration: 'none', color: 'black', textTransform: 'uppercase', fontWeight: location.pathname === '/' ? 'bold' : 'normal' }}>
+              Home
+            </Link>
+          </Typography>
+          <Typography variant="h6" style={{ marginRight: '1rem' }}>
+          <Link to="/favorites" style={{ textDecoration: 'none', color: 'black', textTransform: 'uppercase', fontWeight: location.pathname === '/favorites' ? 'bold' : 'normal' }}>
+              My Favorites
+            </Link>
+          </Typography>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
